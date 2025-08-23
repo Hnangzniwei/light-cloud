da47aafff30ce27e657dfbe7743fc8a4c5dd566e
Hnangzniwei <b135600893@qq.com>
2025-08-23 22:20:20 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
+# 指定 Terraform 和 Provider 版本
 terraform {
+  required_version = ">= 1.0"
+
   required_providers {
     tencentcloud = {
       source  = "tencentcloudstack/tencentcloud"
-      version = "1.13.0"  # 手动指定版本
+      version = "1.13.0"
     }
     aws = {
       source  = "hashicorp/aws"
@@ -11,73 +14,42 @@ terraform {
   }
 }
 
-
-
-
-
-# 全局变量
+# 本地变量：根据 cloud_provider 判断使用哪个云厂商
 locals {
-  cloud_provider = var.cloud_provider
-  is_tencent     = local.cloud_provider == "tencent"
-  is_aws         = local.cloud_provider == "aws"
+  cloud_provider = lower(var.cloud_provider)
+
+  is_tencent = local.cloud_provider == "tencent"
+  is_aws     = local.cloud_provider == "aws"
 }
 
-# Provider 配置
+# 腾讯云 Provider 配置
 provider "tencentcloud" {
-  region       = var.region
-  secret_id    = var.TENCENT_SECRET_ID
-  secret_key   = var.TENCENT_SECRET_KEY
-  skip_create_service = !local.is_tencent
+  region = var.region
+
+  secret_id  = var.TENCENT_SECRET_ID
+  secret_key = var.TENCENT_SECRET_KEY
+
+  # 仅当选择腾讯云时启用
+  count = local.is_tencent ? 1 : 0
 }
 
+# AWS Provider 配置
 provider "aws" {
-  region  = var.region
-  assume_role = local.is_aws ? null : {
-    role_arn      = "arn:aws:iam::000000000000:role/NonExistentRole"
-    session_name  = "SkipProvider"
+  region = var.region
+
+  # 可选：假设角色，用于跨账户访问
+  assume_role {
+    # 只有在 is_aws 为 true 且提供了 role_arn 时才启用
+    role_arn = local.is_aws && var.AWS_ASSUME_ROLE_ARN != "" ? var.AWS_ASSUME_ROLE_ARN : null
   }
+
+  count = local.is_aws ? 1 : 0
 }
 
-# 变量定义
-variable "cloud_provider" {
-  description = "选择云厂商: 'tencent' 或 'aws'"
-  type        = string
-  default     = "tencent"
-}
-
-variable "region" {
-  description = "云区域, 如 ap-beijing / us-east-1"
-  type        = string
-  default     = "ap-beijing"
-}
-
-variable "TENCENT_SECRET_ID" {
-  description = "腾讯云 SecretId"
-  type        = string
-  sensitive   = true
-}
-
-variable "TENCENT_SECRET_KEY" {
-  description = "腾讯云 SecretKey"
-  type        = string
-  sensitive   = true
-}
-
-variable "instance_password" {
-  description = "Lighthouse 实例登录密码（8-30位, 含大小写、数字、特殊字符）"
-  type        = string
-  sensitive   = true
-}
-
-variable "aws_key_name" {
-  description = "AWS EC2 密钥对名称"
-  type        = string
-  default     = "mykey"
-}
-
-# Tencent Cloud Lighthouse 实例
-resource "tencentcloud_lighthouse_instance" "lighthouse_instance" {
+# 腾讯云 Lighthouse 实例（轻量应用服务器）
+resource "tencentcloud_lighthouse_instance" "lighthouse" {
   count = local.is_tencent ? 1 : 0
 
-  zone = "<LaTex>${var.region}-1"[ty-n]  # 其他配置...[ty-n]}[ty-n][ty-n]# AWS EC2 实例[ty-n]resource "aws\_instance" "ec2\_instance" {[ty-n]  count = local.is\_aws ? 1 : 0[ty-n][ty-n]  ami           = "ami-0c55b159cbfafe1f0"[ty-n]  instance\_type = "t2.micro"[ty-n]  key\_name      = var.aws\_key\_name[ty-n][ty-n]  # 其他配置...[ty-n]}[ty-n][ty-n]# 安全组规则[ty-n]resource "aws\_security\_group" "sg" {[ty-n]  count = local.is\_aws ? 1 : 0[ty-n][ty-n]  name        = "allow\_ssh\_and\_http"[ty-n]  description = "Allow SSH and HTTP"[ty-n][ty-n]  ingress {[ty-n]    from\_port   = 22[ty-n]    to\_port     = 22[ty-n]    protocol    = "tcp"[ty-n]    cidr\_blocks = ["0.0.0.0/0"][ty-n]  }[ty-n][ty-n]  ingress {[ty-n]    from\_port   = 80[ty-n]    to\_port     = 80[ty-n]    protocol    = "tcp"[ty-n]    cidr\_blocks = ["0.0.0.0/0"][ty-n]  }[ty-n][ty-n]  egress {[ty-n]    from\_port   = 0[ty-n]    to\_port     = 0[ty-n]    protocol    = "-1"[ty-n]    cidr\_blocks = ["0.0.0.0/0"][ty-n]  }[ty-n]}[ty-n][ty-n]# 输出[ty-n]output "public\_ip" {[ty-n]  value = concat(tencentcloud\_lighthouse\_instance.lighthouse\_instance.*.public\_ip, aws\_instance.ec2\_instance.*.public\_ip)[0][ty-n]}[ty-n][ty-n]output "ssh\_command" {[ty-n]  value = "ssh root@$</LaTex>{element(concat(tencentcloud_lighthouse_instance.lighthouse_instance.*.public_ip, aws_instance.ec2_instance.*.public_ip), 0)}"
+  availability_zone = lookup(var.tencent_zones, var.region, "<LaTex>${var.region}-1")[ty-n]  blueprint\_id      = var.tencent\_blueprint\_id[ty-n]  bundle\_id         = var.tencent\_bundle\_id[ty-n]  instance\_name     = var.instance\_name[ty-n][ty-n]  # 密码需满足复杂度要求[ty-n]  login\_password = var.instance\_password[ty-n][ty-n]  # 是否自动续费[ty-n]  renew\_flag = var.tencent\_renew\_flag[ty-n]}[ty-n][ty-n]# AWS EC2 实例（作为对比示例）[ty-n]resource "aws\_instance" "web" {[ty-n]  count = local.is\_aws ? 1 : 0[ty-n][ty-n]  ami           = var.AWS\_AMI\_ID[ty-n]  instance\_type = var.AWS\_INSTANCE\_TYPE[ty-n]  key\_name      = var.aws\_key\_name[ty-n]  subnet\_id     = var.AWS\_SUBNET\_ID[ty-n]  vpc\_security\_group\_ids = var.AWS\_SECURITY\_GROUP\_IDS[ty-n][ty-n]  tags = {[ty-n]    Name = "$</LaTex>{var.instance_name}-aws"
+  }
 }
